dd7e6539c4ed50cf2f73b51dd4596b93f71ca798
Hugo <hugo.duperthuy@outlook.fr>
2025-01-15 09:09:42 +0100

    ajout front my-acount recipe

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccountController extends AbstractController
 {
@@ -22,4 +23,34 @@ class AccountController extends AbstractController
             'recipes' => $recipes,
         ]); 
     }
+
+    #[Route('/delete-recipe/{id}', name: 'delete_recipe', methods: ['POST'])]
+    public function deleteRecipe(int $id, EntityManagerInterface $entityManager): Response
+    {
+        // connected user
+        $user = $this->getUser();
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour supprimer une recette.');
+        }
+
+        // pull recipe
+        $recipe = $entityManager->getRepository(RecipeType::class)->find($id);
+        if (!$recipe) {
+            throw $this->createNotFoundException('Recette introuvable.');
+        }
+
+        // user == author ?
+        if ($recipe->getAuthor() !== $user) {
+            throw $this->createAccessDeniedException('Vous ne pouvez pas supprimer cette recette.');
+        }
+
+        // delete recipe
+        $entityManager->remove($recipe);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La recette a été supprimée avec succès.');
+
+        return $this->redirectToRoute('account');
+    }
+
 }
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -14,46 +14,44 @@ use Symfony\Component\Routing\Annotation\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/new-recipe', name: 'new_recipe')]
-public function ecrireRecette(Request $request, EntityManagerInterface $entityManager): Response
-{
-    $recipe = new Recipe();
+    public function ecrireRecette(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $recipe = new Recipe();
 
-    // import du form
-    $form = $this->createForm(RecipeType::class, $recipe);
-    $form->handleRequest($request);
+        // import du form
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-        // infos user
-        $user = $this->getUser();
+        if ($form->isSubmitted() && $form->isValid()) {
+            // infos user
+            $user = $this->getUser();
 
-        // user not connected
-        if (!$user) {
-            throw $this->createAccessDeniedException('Vous devez être connecté pour créer une recette.');
+            // user not connected
+            if (!$user) {
+                throw $this->createAccessDeniedException('Vous devez être connecté pour créer une recette.');
+            }
+
+            // define author
+            $recipe->setAuthor($user);
+
+            // persist recipe
+            $entityManager->persist($recipe);
+
+            // persist RecipeIngredients
+            foreach ($recipe->getIngredients() as $recipeIngredient) {
+                $recipeIngredient->setRecipe($recipe);
+                $entityManager->persist($recipeIngredient);
+            }
+
+            // registration
+            $entityManager->flush();
+
+            // redirect
+            return $this->redirectToRoute('home');
         }
 
-        // define author
-        $recipe->setAuthor($user);
-
-        // persist recipe
-        $entityManager->persist($recipe);
-
-        // persist RecipeIngredients
-        foreach ($recipe->getIngredients() as $recipeIngredient) {
-            $recipeIngredient->setRecipe($recipe);
-            $entityManager->persist($recipeIngredient);
-        }
-
-        // registration
-        $entityManager->flush();
-
-        // redirect
-        return $this->redirectToRoute('home');
+        return $this->render('recipe/new_recipe.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
-
-    return $this->render('recipe/new_recipe.html.twig', [
-        'form' => $form->createView(),
-    ]);
-}
-
-
 }
